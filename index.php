bedc043b7aa7c79ee6aac21bf47f803c496cb9bb
chanjal@28 <chanjalnc1999@gmail.com>
2020-11-30 20:49:27 +0530

    product information

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Product Information</h1>
+
+    <table class="table">
+        <tr>
+            <td>Product Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+
+        <tr>
+            <td>Product manufacture</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+
+        
+        <tr>
+            <td>Price</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+
+        
+        <tr>
+            <td>Distributor Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+
+        
+        <tr>
+            <td>Retailer Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button class="btn btn-success">Submitt</button></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
